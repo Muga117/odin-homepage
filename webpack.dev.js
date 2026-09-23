274c6b6494b15e2d3e971b4967ea189db91ba006
Muga Karmi Mugiisa Otieno <exiagardofficial@gmail.com>
2026-09-23 11:32:28 +0300

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+    ],
+  },
+});
